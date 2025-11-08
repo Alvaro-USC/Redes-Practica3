31358c7721f3077e90d81902a85646c789a3211b
Álvaro <alvaro.schwiedop@rai.usc.es>
2025-11-08 12:53:18 +0100

    Update emisor.c

--- emisor.c
+++ emisor.c
@@ -6,7 +6,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define N_FLOATS 1000
+#define MAX_SIZE 1024
 /* Función para imprimir errores */
 void error(const char *msg) {
     perror(msg);
@@ -24,31 +24,32 @@ int main(int argc, char *argv[]) {
     int puerto_destino = atoi(argv[3]);
     char* ip_destino = argv[2]; 
 
-    float mensaje [N_FLOATS];
+    char mensaje [MAX_SIZE];
 
     size_t sizeMssg = sizeof(mensaje);
-    
-    for (int i = 0; i < N_FLOATS; ++i)
-    {
-        mensaje[i] = 2.5 * (i+2);
-        printf("Mensaje[%d]: %f\n", i, mensaje[i]);
-    }
 
     if (puerto_propio <= IPPORT_USERRESERVED || puerto_propio > 65535) {
         fprintf(stderr, "Numero de puerto_propio invalido: %s\n", argv[1]);
-        printf("Escuchando al puerto por defecto: 8000");
+        printf("Escuchando al puerto por defecto: 7000\n");
         puerto_propio = 7000;   
     }
 
     if (puerto_destino <= IPPORT_USERRESERVED || puerto_destino > 65535) {
         fprintf(stderr, "Numero de puerto_destino invalido: %s\n", argv[1]);
-        printf("Escuchando al puerto por defecto: 8000");
+        printf("Escuchando al puerto por defecto: 8000\n");
         puerto_destino = 8000;   
     }
 
     /* Creamos el socket, protocolo 0 para cada par dominio/tipo */
     int emisor_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (emisor_fd < 0) error("Error en creacion del Socket");
+
+    printf("Introduce el mensaje a enviar: ");
+    if (fgets(mensaje, sizeMssg, stdin) == NULL) {
+        fprintf(stderr, "Error al leer el mensaje\n");
+        close(emisor_fd);
+        exit(EXIT_FAILURE);
+    }
 
     /* Configurar address del servidor */
     struct sockaddr_in socket_propio;
@@ -62,7 +63,15 @@ int main(int argc, char *argv[]) {
     socket_remoto.sin_family = AF_INET;
     int retIP = inet_pton(AF_INET, ip_destino, &socket_remoto2); /* Escuchar a todas las interfaces */
     socket_remoto.sin_addr.s_addr = socket_remoto2.s_addr;
-    if (retIP < 0) error("inet_pton fallo");
+    if (retIP == 0) {
+        fprintf(stderr, "Error: dirección IP destino inválida: %s\n", ip_destino);
+        close(emisor_fd);
+        exit(EXIT_FAILURE);
+    } else if (retIP < 0) {
+        perror("Error en inet_pton");
+        close(emisor_fd);
+        exit(EXIT_FAILURE);
+    }
     socket_remoto.sin_port = htons(puerto_destino); /* Traducir orden de máquina a orden de red */
 
     /* Bind del socket a la address, porque cada socket tiene que tener una dirección */
@@ -70,7 +79,7 @@ int main(int argc, char *argv[]) {
         error("Bind fallo");
 
     socklen_t tamSocketRemoto = sizeof(socket_remoto);
-    ssize_t bytesEnv = sendto(emisor_fd, mensaje, sizeMssg, 0, (struct sockaddr *)&socket_remoto, tamSocketRemoto);
+    ssize_t bytesEnv = sendto(emisor_fd, mensaje, strlen(mensaje), 0, (struct sockaddr *)&socket_remoto, tamSocketRemoto);
     if (bytesEnv < 0) error("sendto fallo");
     
     printf("Bytes enviados: %ld\n", bytesEnv);
